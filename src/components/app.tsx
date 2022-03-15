5805df92d70ad6f2e0725918620f1bc571149eb9
Warren Hong <hwhong1129@gmail.com>
2022-03-14 22:44:25 -0700

    overlay grid on cavnas

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -6,9 +6,53 @@ import { ActionBar } from "./action-bar";
 
 export function App() {
   const color = useSelector((state: PixelRootState) => state.colors);
+  const canvasRef = React.useRef<HTMLCanvasElement | null>(null);
+
+  React.useEffect(() => {
+    const canvas = canvasRef.current;
+
+    if (canvas) {
+      // Make it visually fill the positioned parent
+      canvas.style.width = "100%";
+      canvas.style.height = "100%";
+      // ...then set the internal size to match
+      canvas.width = canvas.offsetWidth;
+      canvas.height = canvas.offsetHeight;
+    }
+  }, [canvasRef]);
+
   return (
     <div className={styles.root}>
       <ActionBar />
+
+      <div className={styles.parentLayer}>
+        <div className={styles.wrapper}>
+          {Array.from(Array(10).keys()).map((y) =>
+            Array.from(Array(10).keys()).map((x) => (
+              <div
+                key={y}
+                className={styles.gridItem}
+                onClick={() => {
+                  const canvas = canvasRef.current;
+                  if (canvas?.getContext) {
+                    const ctx = canvas.getContext("2d")!;
+                    ctx.beginPath();
+                    ctx.rect(x * 50, y * 50, 50, 50);
+                    ctx.stroke();
+                  }
+                }}
+              >{`${x}-${y}`}</div>
+            ))
+          )}
+        </div>
+        <canvas
+          ref={canvasRef}
+          onMouseDown={(e) => console.log(e)}
+          //onMouseMove={(e) => console.log(e)}
+          className={styles.canvas}
+          //onClick={(e) => console.log(e)}
+        />
+      </div>
     </div>
   );
 }
