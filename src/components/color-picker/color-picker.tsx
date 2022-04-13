c90e518887926f5d2f5701f153205ef97596030a
Warren Hong <hwhong1129@gmail.com>
2022-04-12 22:36:15 -0700

    thank you google material ui

--- src/components/color-picker/color-picker.tsx
+++ src/components/color-picker/color-picker.tsx
@@ -5,50 +5,67 @@ import { useDispatch, useSelector } from "react-redux";
 import { addColor, setColor } from "../../actions/pixels";
 import { StateType } from "../../app/store";
 import styles from "./color-picker.module.css";
+import Popover from "@mui/material/Popover";
 
 export function ColorPicker() {
   const dispatch = useDispatch();
-  const [isOpen, setIsOpen] = React.useState<boolean>(false);
   const [selectedColor, setSelectedColor] = React.useState<string>();
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(
+    null
+  );
   const currentColor = useSelector((state: StateType) => state.pixel.color);
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <div>
       <button
-        className={classNames(styles.add, { [styles.open]: isOpen })}
+        aria-describedby={id}
+        className={classNames(styles.add, { [styles.open]: open })}
         onClick={(e) => {
           e.stopPropagation();
-          setIsOpen(true);
+          setAnchorEl(e.currentTarget);
         }}
       />
-      {isOpen ? (
-        <div className={styles.popover}>
-          <div className={styles.cover} onClick={() => setIsOpen(false)} />
-          <div className={styles.wrapper}>
-            <ChromePicker
-              color={currentColor}
-              onChangeComplete={({ hex }, e) => {
-                e.preventDefault();
-                e.stopPropagation();
-                dispatch(setColor(hex));
-                setSelectedColor(hex);
-              }}
-            />
-            <button
-              className={styles.addButton}
-              onClick={() => {
-                if (selectedColor) {
-                  dispatch(setColor(selectedColor));
-                  dispatch(addColor(selectedColor));
-                  setIsOpen(false);
-                }
-              }}
-            >
-              Add
-            </button>
-          </div>
+      <Popover
+        open={open}
+        id={id}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "center",
+          horizontal: "right",
+        }}
+      >
+        <div className={styles.wrapper}>
+          <ChromePicker
+            color={currentColor}
+            onChangeComplete={({ hex }, e) => {
+              e.preventDefault();
+              e.stopPropagation();
+              dispatch(setColor(hex));
+              setSelectedColor(hex);
+            }}
+          />
+          <button
+            className={styles.addButton}
+            onClick={() => {
+              if (selectedColor) {
+                dispatch(setColor(selectedColor));
+                dispatch(addColor(selectedColor));
+                setAnchorEl(null);
+              }
+            }}
+          >
+            Add
+          </button>
         </div>
-      ) : null}
+      </Popover>
     </div>
   );
 }
