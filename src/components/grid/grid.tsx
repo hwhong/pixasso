d8f6a0f652d6f8c4340de70ed0e322b61c4eee89
Warren Hong <hwhong1129@gmail.com>
2022-04-15 18:08:38 -0700

    fingers crossed

--- src/components/grid/grid.tsx
+++ src/components/grid/grid.tsx
@@ -37,8 +37,8 @@ export function Grid({ canvasRef }: GridProps) {
 
     if (canvas) {
       // Make it visually fill the positioned parent
-      canvas.style.width = "100%";
-      canvas.style.height = "100%";
+      canvas.style.width = "640px";
+      canvas.style.height = "640px";
       // ...then set the internal size to match
       canvas.width = canvas.offsetWidth;
       canvas.height = canvas.offsetHeight;
