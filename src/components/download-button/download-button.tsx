9d5d236aa21a75143daa239422dcb86b5f43becb
Warren Hong <hwhong1129@gmail.com>
2022-04-17 20:49:16 -0700

    feature: to specify name of the image file

--- src/components/download-button/download-button.tsx
+++ src/components/download-button/download-button.tsx
@@ -1,25 +1,67 @@
 import React from "react";
 import styles from "./download-button.module.css";
+import Box from "@mui/material/Box";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import Modal from "@mui/material/Modal";
+import classNames from "classnames";
+import { modalStyle } from "../pixasso-modal/helper";
+import Typography from "@mui/material/Typography";
 
 interface DownloadButtonProps {
   canvasRef: React.MutableRefObject<HTMLCanvasElement | null>;
 }
 
 export function DownloadButton({ canvasRef }: DownloadButtonProps) {
+  const [open, setOpen] = React.useState(false);
+  const [filename, setFilename] = React.useState("");
+  const handleClose = () => setOpen(false);
+
   return (
-    <button
-      className={styles.root}
-      onClick={() => {
-        const canvas = canvasRef.current;
-        if (canvas) {
-          var link = document.createElement("a");
-          link.download = "filename.png";
-          link.href = canvas.toDataURL();
-          link.click();
-        }
-      }}
-    >
-      Download
-    </button>
+    <>
+      <button
+        className={styles.root}
+        onClick={() => {
+          setOpen(true);
+        }}
+      >
+        Download
+      </button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={modalStyle}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Enter the name of the image file
+          </Typography>
+          <OutlinedInput
+            className={styles.input}
+            size="small"
+            value={filename}
+            placeholder="Name"
+            onChange={({ target }) => setFilename(target.value)}
+          />
+          <div className={styles.buttonsAction}>
+            <button
+              className={classNames(styles.button, styles.yesButton)}
+              onClick={() => {
+                const canvas = canvasRef.current;
+                if (canvas) {
+                  var link = document.createElement("a");
+                  link.download = `${filename}.png`;
+                  link.href = canvas.toDataURL();
+                  link.click();
+                }
+                setOpen(false);
+              }}
+            >
+              Confirm
+            </button>
+          </div>
+        </Box>
+      </Modal>
+    </>
   );
 }
--- src/components/pixasso-modal/pixasso-modal.tsx
+++ src/components/pixasso-modal/pixasso-modal.tsx
@@ -4,6 +4,7 @@ import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import classNames from "classnames";
 import styles from "./pixasso-modal.module.css";
+import { modalStyle } from "./helper";
 
 interface ModalProps {
   description: string;
@@ -25,20 +26,6 @@ export function PixassoModal({
 }: ModalProps) {
   const handleClose = () => setOpen(false);
 
-  const style = {
-    position: "absolute" as "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: 400,
-    bgcolor: "background.paper",
-    border: "2px solid #000",
-    boxShadow: 24,
-    display: "flex",
-    flexDirection: "column",
-    p: 4,
-  };
-
   return (
     <Modal
       open={open}
@@ -46,7 +33,7 @@ export function PixassoModal({
       aria-labelledby="modal-modal-title"
       aria-describedby="modal-modal-description"
     >
-      <Box sx={style}>
+      <Box sx={modalStyle}>
         <Typography id="modal-modal-title" variant="h6" component="h2">
           {title}
         </Typography>
