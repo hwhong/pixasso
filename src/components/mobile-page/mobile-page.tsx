c3fac0107b4543f396f88a78f416f18f61ec1def
Warren Hong <hwhong1129@gmail.com>
2022-06-29 22:30:32 -0700

    padding

--- src/components/mobile-page/mobile-page.tsx
+++ src/components/mobile-page/mobile-page.tsx
@@ -1,5 +1,8 @@
 import React from "react";
+import styles from "./mobile-page.module.css";
 
 export function MobilePage() {
-  return <h2>Use a desktop browser to use the platform.</h2>;
+  return (
+    <h2 className={styles.root}>Use a desktop browser to use the platform.</h2>
+  );
 }
